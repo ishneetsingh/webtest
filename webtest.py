bd6b0706079ba44eb31d2c8a84c8d38f4a1d57db
Ishneet0710 <85265554+Ishneet0710@users.noreply.github.com>
2022-11-21 14:04:58 +0800

    Update webtest.py

--- webtest.py
+++ webtest.py
@@ -1,8 +1,33 @@
 from streamlit_webrtc import webrtc_streamer
 
-webrtc_streamer(key="sexy boy",
-                rtc_configuration={
-                    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-                }
-)
-
+webrtc_streamer(
+        key="real-time",
+        video_frame_callback=callback,
+        media_stream_constraints={
+            "video": True,
+            "audio": False
+        },
+        # For Deploying
+        rtc_configuration={
+             "iceServers": [
+            {
+              "urls": "stun:openrelay.metered.ca:80",
+            },
+            {
+              "urls": "turn:openrelay.metered.ca:80",
+              "username": "openrelayproject",
+              "credential": "openrelayproject",
+            },
+            {
+              "urls": "turn:openrelay.metered.ca:443",
+              "username": "openrelayproject",
+              "credential": "openrelayproject",
+            },
+            {
+              "urls": "turn:openrelay.metered.ca:443?transport=tcp",
+              "username": "openrelayproject",
+              "credential": "openrelayproject",
+            },
+          ]
+        }
+    )
