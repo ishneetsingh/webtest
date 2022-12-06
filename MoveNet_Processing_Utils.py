a6ee741c308c184bef9a693fa35d32c2fe2b23c5
Ishneet0710 <85265554+Ishneet0710@users.noreply.github.com>
2022-12-07 01:34:58 +0800

    Update MoveNet_Processing_Utils.py

--- MoveNet_Processing_Utils.py
+++ MoveNet_Processing_Utils.py
@@ -51,7 +51,8 @@ def load_movenet():
 MOVENET = load_movenet()
 
 # Significant Features
-IDX_TO_KEEP = [42,50, 39, 29, 32, 30, 27, 37, 25, 19, 31, 36, 33, 13, 43,  7,  1, 34,  4, 10, 16, 49, 28, 22, 40, 46]
+IDX_TO_KEEP = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+
 
 def get_affine_transform_to_fixed_sizes_with_padding(size, new_sizes):
     width, height = new_sizes
