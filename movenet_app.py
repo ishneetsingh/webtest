4e35e897069664c5cea3a5318ad504a37c401b4e
ishneetsingh <124784915+ishneetsingh@users.noreply.github.com>
2023-06-04 16:41:25 +0800

    Update movenet_app.py

--- movenet_app.py
+++ movenet_app.py
@@ -6,7 +6,38 @@ import av
 from PIL import Image
 import numpy as np
 
+import logging
+import os
 
+from twilio.rest import Client
+
+logger = logging.getLogger(__name__)
+
+
+@st.cache_data
+def get_ice_servers():
+    """Use Twilio's TURN server because Streamlit Community Cloud has changed
+    its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
+    We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
+    but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
+    See https://github.com/whitphx/streamlit-webrtc/issues/1213
+    """
+
+    # Ref: https://www.twilio.com/docs/stun-turn/api
+    try:
+        account_sid = os.environ[ACd7310e779f963a968a1018bac2926c22]
+        auth_token = os.environ[a088ac0c10c87ce2c704810ffcf8db18]
+    except KeyError:
+        logger.warning(
+            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    client = Client(account_sid, auth_token)
+
+    token = client.tokens.create()
+
+    return token.ice_servers
 
 DEMO_IMAGE = "./demos/demo.jpg"
 
